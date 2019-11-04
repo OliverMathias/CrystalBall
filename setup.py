79eaf0631907056aa8611119e0d406174992858b
MuskCoin <34895244+MuskCoin@users.noreply.github.com>
2019-11-04 10:48:48 -0800

    push for new readme on pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
-from distutils.core import setup
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
   name = 'Predictionary',         # How you named your package folder (MyLib)
   packages = ['Predictionary'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.1.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A python package that allows for the easy creation of LSTM text prediction datasets',   # Give a short description about your library
   author = 'Oliver Mathias',                   # Type in your name
   author_email = 'mathias@chapman.edu',      # Type in your E-Mail
   url = 'https://github.com/OliverMathias/Predictionary',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/OliverMathias/Predictionary/archive/v0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/OliverMathias/Predictionary/archive/v0.1.1.tar.gz',    # I explain this later on
   keywords = ['LSTM', 'pipeline', 'data', 'text', 'prediction'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'tika',
@@ -28,4 +32,7 @@ setup(
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.7',
   ],
+
+  long_description=long_description,
+   long_description_content_type="text/markdown"
 )
